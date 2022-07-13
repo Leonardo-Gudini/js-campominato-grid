f0373d4d31829d6257a22bc9516a0df360ee6a59
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-07-13 17:32:54 +0200

    creata griglia campo minato

---
+++ js/main.js
@@ -0,0 +1,10 @@
+for(let i=0; i<100; i++){
+    const campContainer = document.getElementById("container");
+    let box = document.createElement("div")
+    box.classList.add("squareBox")
+
+    campContainer.append(box);
+}
+
+
+
