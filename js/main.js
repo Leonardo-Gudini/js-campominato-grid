88344c7e3968b7ab48c910251245d52fe6c90217
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-07-13 18:43:22 +0200

    add numeri casuali

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
-for(let i=0; i<100; i++){
-    const campContainer = document.getElementById("container");
+let campContainer = document.getElementById("container");
+
+let numberOfBox = 100;
+
+for(let i = 0; i < numberOfBox; i++){
+
+    const numeroBox = casualNumber(1,100)
+    const squareBox = addSquareBox(numeroBox)
+
+    campContainer.append(squareBox);
+}
+
+
+
+function casualNumber (min,max){
+    let number = Math.floor(Math.random() * ( max - min + 1)) + min;
+    console.log(number);
+    return number;
+}
+
+
+
+
+
+
+function addSquareBox (numero){
+    
     let box = document.createElement("div")
     box.classList.add("squareBox")
 
-    campContainer.append(box);
+    box.append(numero);
+    return box;
 }
-
 
 
